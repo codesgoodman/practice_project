07d47027e5a24cab5210ba9a647e19a23541bbdf
Kihyun Kim <rlgus3689@gmail.com>
2023-07-04 01:50:09 +1200

    .

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import AddUser from "./components/AddUser/AddUser";
 import UsersList from "./components/UsersList/UsersList";
 import "./index.css";
 
 function App() {
+  const [isValid, setIsValid] = useState(true);
+  const [userInput, setUserInput] = useState("");
   function addUserHandler(userInfo) {
-    console.log(userInfo);
+    if (userInfo[0].length > 1 || userInfo[1] > 1) {
+      setUserInput(userInfo);
+    } else {
+      setIsValid(false);
+    }
   }
   return (
     <div>
       <AddUser onAddUser={addUserHandler} />
+      <UsersList data={userInput} validity={isValid} />
     </div>
   );
 }
--- src/components/UsersList/UsersList.js
+++ src/components/UsersList/UsersList.js
@@ -1,7 +1,12 @@
 import React from "react";
+import classes from "./UsersList.module.css";
 
-function UsersList() {
-  return <div></div>;
+function UsersList(props) {
+  return (
+    <div>
+      <p>{`${props.data[0]}(${props.data[1]})`}</p>
+    </div>
+  );
 }
 
 export default UsersList;
