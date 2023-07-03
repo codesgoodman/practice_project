82dda988dcbe318431d320e9a75eadb36243426a
Kihyun Kim <rlgus3689@gmail.com>
2023-07-04 00:30:03 +1200

    lift AddUser state up to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import AddUser from "./components/AddUser/AddUser";
+import UsersList from "./components/UsersList/UsersList";
 import "./index.css";
 
 function App() {
+  function addUserHandler(userInfo) {
+    console.log(userInfo);
+  }
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={addUserHandler} />
     </div>
   );
 }
--- src/components/AddUser/AddUser.js
+++ src/components/AddUser/AddUser.js
@@ -1,20 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./AddUser.module.css";
 import Button from "../Button/Button";
 
-function AddUser() {
-  const inputChangeHandler = (event) => {
-    console.log(event.target.value);
+function AddUser(props) {
+  const [inputValue1, setInputValue1] = useState("");
+  const [inputValue2, setInputValue2] = useState("");
+  const inputChangeHandler1 = (event) => {
+    setInputValue1(event.target.value);
+  };
+  const inputChangeHandler2 = (event) => {
+    setInputValue2(event.target.value);
   };
   const submitHandler = (event) => {
     event.preventDefault();
+    props.onAddUser([inputValue1, inputValue2]);
   };
   return (
     <form className={classes.input} onSubmit={submitHandler}>
       <p>User name</p>
-      <input onChange={inputChangeHandler} type="text"></input>
+      <input onChange={inputChangeHandler1} type="text"></input>
       <p>Age(Years)</p>
-      <input onChange={inputChangeHandler} type="number"></input>
+      <input onChange={inputChangeHandler2} type="number"></input>
       <Button />
     </form>
   );
