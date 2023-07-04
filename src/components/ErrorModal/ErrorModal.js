b3e5011dcda4d1091b0dcc1bf9fcd5f0724d646f
Kihyun Kim <rlgus3689@gmail.com>
2023-07-04 18:53:28 +1200

    implement errorModal

--- src/components/ErrorModal/ErrorModal.js
+++ src/components/ErrorModal/ErrorModal.js
@@ -1,7 +1,22 @@
 import React from "react";
+import Card from "../Card/Card";
+import Button from "../Button/Button";
+import classes from "./ErrorModal.module.css";
 
-function ErrorModal() {
-  return <div></div>;
+function ErrorModal(props) {
+  return (
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{[props.title]}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button>Okay</Button>
+      </footer>
+    </Card>
+  );
 }
 
 export default ErrorModal;
